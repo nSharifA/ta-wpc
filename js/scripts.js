6931a04a63c4c25d5847e164f626d9e2b4f8dd3c
nSharifA <n.sharif@windowslive.com>
2022-04-02 23:31:51 +0200

    scrolling navigation 1/2

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+const sections = [
+    ...document.querySelectorAll('main section')
+];
+
+const navItems = {};
+[...document.querySelectorAll('nav a')].forEach(item => {
+    navItems[item.dataset.for] = item;
+});
+
+// intersection observer setup
+const observerOptions = {
+    root: null,
+    rootMargin: '15%',
+    threshold: 0.7,
+  };
+  
+  function observerCallback(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // get the nav item corresponding to the id of the section
+        // that is currently in view
+        const navItem = navItems[entry.target.id];
+        // add 'active' class on the navItem
+        navItem.classList.add('active');
+        // remove 'active' class from any navItem that is not
+        // same as 'navItem' defined above
+        Object.values(navItems).forEach((item) => {
+          if (item != navItem) {
+            item.classList.remove('active');
+          }
+        });
+      }
+    });
+  }
+  
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  
+  sections.forEach((sec) => observer.observe(sec));
+  
