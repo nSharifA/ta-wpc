539c634335169c1ca2301ed1dd522264b7edcc9b
nSharifA <n.sharif@windowslive.com>
2022-04-04 19:44:34 +0200

    add opacity animation to mobile, close menu on navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,48 @@
 const sections = [
-    ...document.querySelectorAll('main section')
+  ...document.querySelectorAll('main section')
 ];
 
 const navItems = {};
 [...document.querySelectorAll('nav a')].forEach(item => {
-    navItems[item.dataset.for] = item;
+  navItems[item.dataset.for] = item;
 });
 
 // intersection observer setup
 const observerOptions = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.5,
-  };
-  
-  function observerCallback(entries, observer) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        // get the nav item corresponding to the id of the section
-        // that is currently in view
-        const navItem = navItems[entry.target.id];
-        // add 'active' class on the navItem
-        navItem.classList.add('active');
-        entry.target.classList.add('show');
-        // remove 'active' class from any navItem that is not
-        // same as 'navItem' defined above
-        Object.values(navItems).forEach((item) => {
-          if (item != navItem) {
-            item.classList.remove('active');
-          }
-        });
-      }
-    });
-  }
-  
-  const observer = new IntersectionObserver(observerCallback, observerOptions);
-  
-  sections.forEach((sec) => observer.observe(sec));
+  root: null,
+  rootMargin: '0px',
+  threshold: [0.25, 0.5, 0.75],
+};
+
+let prevIntersectRatio = 0;
+
+let menuToggleCb = document.getElementById('nav-toggle');
+
+function observerCallback(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0.25 && 0.5 > entry.intersectionRatio) {
+      // get the nav item corresponding to the id of the section
+      // that is currently in view
+      const navItem = navItems[entry.target.id];
+      // add 'active' class on the navItem
+      navItem.classList.add('active');
+      window.history.pushState(entry.target.id, entry.target.id, '#' + entry.target.id);
+      // remove 'active' class from any navItem that is not
+      // same as 'navItem' defined above
+      Object.values(navItems).forEach((item) => {
+        if (item != navItem) {
+          item.classList.remove('active');
+        }
+      });
+    }
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      menuToggleCb.checked = false;
+    }
+    prevIntersectRatio = entry.intersectionRatio;
+  });
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+sections.forEach((sec) => observer.observe(sec));
